f045796f4032ed0afc0c6f19ed611ea1da22cad2
Apple <apple@Apples-MacBook-Air.local>
2025-06-14 09:15:18 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -38,4 +39,7 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
